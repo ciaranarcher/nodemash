fbfd0f1eb2715faeece1e875158da4f965de6186
Ciaran Archer <ciaran.archer@gmail.com>
2012-10-21 17:06:35 +0100

    Added as module.

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-var twitterSteam = require('./tweetHoover')
+var Twitter = require('./tweetHoover');
+var tweetStream = new Twitter.TweetHoover().stream
+
+// add listeners
+tweetStream.addListener('response', function (response) {
+  response.setEncoding('utf8');
+  response.addListener('data', function (chunk) {
+    console.log(chunk);
+  });
+  response.addListener('end', function () {
+    console.log('--- END ---')
+  });
+});
+
+// close stream
+tweetStream.end();  
+
