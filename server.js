199bc06b31ff104615ecf6f00ffb6ba3f4edc5e8
Ciaran Archer <ciaran.archer@gmail.com>
2012-10-21 22:17:42 +0100

    Tidy up.

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var fs = require('fs');
 var _ = require('underscore');
 var sockets = [];
 
+handleSocketConnections();
+setupTwitterListeners();
 app.listen(8080);
 
 function handler (req, res) {
@@ -20,42 +22,35 @@ function handler (req, res) {
     res.writeHead(200);
     res.end(data);
   });
-}
+};
 
-io.sockets.on('connection', function (socket) {
-  //console.log(socket);
-  sockets.push(socket);
-  //socket.emit('tweet', { hello: 'world' });
-  // socket.on('my other event', function (data) {
-  //   console.log(data);
-  // });
-});
-
-// add listeners
-tweetStream.addListener('response', function (response) {
-  response.setEncoding('utf8');
-  response.addListener('data', function (chunk) {
-    try {
-      var tweet = JSON.parse(chunk);
-      //console.log(tweet.text);
-      _.each(sockets, function(sock) {
-        sock.emit('tweet', { text: tweet.text });
-      });
-
-    } catch (e) {
-      //console.log('error parsing ' + chunk);
-    }
-    
+function handleSocketConnections() {
+  io.sockets.on('connection', function (socket) {
+    sockets.push(socket); // store the socket
   });
-  response.addListener('end', function () {
-    console.log('--- END ---')
+};
+
+function setupTwitterListeners() {
+  tweetStream.addListener('response', function (response) {
+    response.setEncoding('utf8');
+    response.addListener('data', function (chunk) {
+      try {
+        var tweet = JSON.parse(chunk);
+        //console.log(tweet.text);
+        _.each(sockets, function(sock) {
+          sock.emit('tweet', { text: tweet.text });
+        });
+
+      } catch (e) {
+        //console.log('error parsing ' + chunk);
+      }
+      
+    });
+    response.addListener('end', function () {
+      console.log('--- END ---')
+    });
   });
-});
 
-// call end() to start streaming
-tweetStream.end();
-
-
-
- 
-
+  // call end() to start streaming
+  tweetStream.end();
+};
